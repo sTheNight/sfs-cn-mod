9156379b42541dd57ad06636271a36741a925b2e
sTheNight <sthenight@outlook.com>
2026-07-13 14:25:02 +0800

    feat: 优化 SwitchSettingCard 组件，调整 props 定义和事件处理逻辑

--- src/components/setting/SwitchSettingCard.vue
+++ src/components/setting/SwitchSettingCard.vue
@@ -6,16 +6,23 @@ import type { BasicSettingCardProps } from './index.ts';
 interface SwitchSettingCardEmits {
   (e: "onSwitch", value: boolean): void
 }
-const emit = defineEmits<SwitchSettingCardEmits>()
+interface SwitchSettingCardProps {
+  modelValue: boolean
+}
 
-const props = defineProps<BasicSettingCardProps & { modelValue: boolean }>()
+const emit = defineEmits<SwitchSettingCardEmits>()
+const props = defineProps<BasicSettingCardProps & SwitchSettingCardProps>()
+
+function toggle() {
+  emit('onSwitch', !props.modelValue)
+}
 
 function onSelect(payload: boolean) {
   emit('onSwitch', payload)
 }
 </script>
 <template>
-  <BasicSettingCard :title="props.title" :description="props.description">
-    <Switch :model-value="props.modelValue" @update:model-value="onSelect"></Switch>
+  <BasicSettingCard @click="toggle" :title="props.title" :description="props.description">
+    <Switch @click.stop :model-value="props.modelValue" @update:model-value="onSelect"></Switch>
   </BasicSettingCard>
 </template>
