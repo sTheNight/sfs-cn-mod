1da4f2fda0c7a34f4af24a6d279b1976b1c70cfe
sTheNight <sthenight@outlook.com>
2026-08-22 15:42:04 +0800

    feat: 优化 Toast 组件样式，调整过渡效果和阴影，提升用户体验

--- src/components/Toast/ToastHost.vue
+++ src/components/Toast/ToastHost.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 import { toastText, toastVisiable, toastCount } from './useToast';
-
 </script>
 <template>
   <Teleport to="body">
@@ -8,10 +7,12 @@ import { toastText, toastVisiable, toastCount } from './useToast';
       <Transition name="toast-fade" mode="out-in">
         <div v-if="toastVisiable" :key="toastCount" class="mb-16
                  bg-neutral-800/60 dark:bg-neutral-50/60
-                 text-white dark:text-black
+                 text-neutral-50 dark:text-neutral-900
                  pointer-events-auto
                  px-3 py-1.5 text-sm rounded-lg
-                 backdrop-blur-lg">
+                 backdrop-blur-lg
+                 shadow-[0_2px_16px_rgba(0,0,0,0.15)]
+                 dark:shadow-[0_2px_16px_rgba(255,255,255,0.15)]">
           {{ toastText }}
         </div>
       </Transition>
@@ -22,20 +23,24 @@ import { toastText, toastVisiable, toastCount } from './useToast';
 <style scoped>
 .toast-fade-enter-active,
 .toast-fade-leave-active {
-  transition:
-    opacity .2s,
-    transform .2s;
+  transition: all .2s cubic-bezier(0.25, 0.10, 0.22, 1.69);
 }
 
-.toast-fade-enter-from,
+.toast-fade-enter-from {
+  opacity: 0;
+  transform: rotate(-10deg) scale(.9);
+}
+
+.toast-fade-enter-to {
+  opacity: 1;
+}
+
+.toast-fade-leave-from {
+  opacity: 1;
+}
+
 .toast-fade-leave-to {
   opacity: 0;
   transform: scale(.9);
-}
-
-.toast-fade-enter-to,
-.toast-fade-leave-from {
-  opacity: 1;
-  transform: scale(1);
 }
 </style>
